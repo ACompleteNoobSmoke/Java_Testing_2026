63d0259fc838f8d565a465d1c48ddcad66e7f659
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-24 08:00:48 -0600

    Added EmailValidation class & test

---
+++ src/test/java/com/noobsmoke/EmailValidatorTest.java
@@ -0,0 +1,26 @@
+package com.noobsmoke;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailValidatorTest {
+
+    private EmailValidator underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new EmailValidator();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ofonmwan@gmail.com", "a-aroon@yahoo.com", "goofygoober@chumbucket.com"})
+    void canValidateEmail(String email) {
+        boolean actualResult = underTest.test(email.trim());
+        assertTrue(actualResult);
+    }
+
+}
