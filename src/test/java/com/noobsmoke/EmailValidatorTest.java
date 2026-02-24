c309727f32b970bf2b528afceaf2324648779698
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-24 08:06:16 -0600

    Worked on parameterized testing

--- src/test/java/com/noobsmoke/EmailValidatorTest.java
+++ src/test/java/com/noobsmoke/EmailValidatorTest.java
@@ -2,6 +2,7 @@ package com.noobsmoke;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.commons.util.StringUtils;
 
@@ -21,6 +22,16 @@ class EmailValidatorTest {
     void canValidateEmail(String email) {
         boolean actualResult = underTest.test(email.trim());
         assertTrue(actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "jackiechan, false",
+            "hello@acompletenoobsmoke.io, true"
+    })
+    void canValidateEmailMultipleScenario(String email, boolean expected) {
+        boolean actualResult = underTest.test(email);
+        assertEquals(expected, actualResult);
     }
 
 }
