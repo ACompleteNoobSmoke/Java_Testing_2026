77cd0ffc0d15feb68943ed359b9e6906cba30a02
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-26 05:02:06 -0600

    Did the first 5 exercises

--- src/test/java/com/noobsmoke/exercise/Exercise1Test.java
+++ src/test/java/com/noobsmoke/exercise/Exercise1Test.java
@@ -1,9 +1,12 @@
 package com.noobsmoke.exercise;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -42,5 +45,37 @@ public class Exercise1Test {
     void testCountVowels(String name, int expectedVowel) {
         int actualVowels = underTest.countVowels(name);
         assertEquals(expectedVowel, actualVowels);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "S2344, true",
+            "A324, false",
+            "S3423243, false",
+            "S1238, true"
+    })
+    void testValidStudentID(String studentID, boolean expectedResult) {
+        assertEquals(expectedResult, underTest.isValidStudentId(studentID));
+    }
+
+    @Test
+    void testCalculateAverage() {
+        List<Integer> scores = List.of(20, 10, 89, 23, 34, 100, 45);
+        double expectAVG = 45.857142857142854;
+
+        double actualAVG = underTest.calculateAverage(scores);
+        assertEquals(expectAVG, actualAVG);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "Jackie Chan, jchan",
+            "Naruto Uzumaki, nuzumaki",
+            "Rock Lee, rlee"
+    })
+    void testGenerateUsername(String fullName, String expectedUsername) {
+        String actualUsername = underTest.generateUsername(fullName);
+        assertEquals(expectedUsername, actualUsername);
     }
 }
